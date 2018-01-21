9fe1ae09ffe7d8acf21da25a604e97690353ce1f
mechanizzed <dsibr@hotmail.com>
2018-01-21 14:39:19 -0200

    Updated Header styles. Added vector-icons. Logout user and redirect to welcome page

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { StackNavigator, TabNavigator } from 'react-navigation';
+import Header from './components/Header';
 
 import Welcome from './pages/welcome';
 import Repositories from './pages/repositories';
@@ -15,6 +17,9 @@ const createRootNavigator = (userExists = false) =>
     },
   }, {
     initialRouteName: userExists ? 'User' : 'Welcome',
+    navigationOptions: {
+      header: props => <Header {...props} />,
+    },
   });
 
 export default createRootNavigator;
