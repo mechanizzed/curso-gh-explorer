c8780b9235817aa14be58864f247a20e4b46d2f8
mechanizzed <dsibr@hotmail.com>
2018-01-21 16:29:50 -0200

    Implemented api with repository component

--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, AsyncStorage, ActivityIndicator } from 'react-native';
+import api from '../../services/api';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Repository from './components/Repository';
@@ -13,10 +14,41 @@ export default class Repositories extends Component {
     ),
   };
 
+  state = {
+    repositories: [],
+    loading: false,
+  };
+
+  componentWillMount() {
+    this.loadRepositories();
+  }
+
+  loadRepositories = async () => {
+    this.setState({ loading: true });
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const response = await api.get(`/users/${username}/repos`);
+    this.setState({ repositories: response.data, loading: false });
+  };
+
+  renderRepositories = () => (
+    this.state.repositories.map(repository => (
+      <Repository key={repository.id} repository={repository} />
+    ))
+  );
+
+  renderList = () => (
+    this.state.repositories.length
+      ? this.renderRepositories()
+      : <Text style={styles.empty}>Nenhum repositório encontrado.</Text>
+  );
+
   render() {
     return (
       <View style={styles.container}>
-        <Repository />
+        { this.state.loading
+          ? <ActivityIndicator size="small" color="#999" />
+          : this.renderList()
+        }
       </View>
     );
   }
